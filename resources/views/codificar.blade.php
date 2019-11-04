3d21c64782323565b444e2594f1026b5684b0d2c
Luana-Oliveira <luana-deoliveira@outlook.com>
2019-11-04 18:52:32 -0300

    modificações script

--- resources/views/codificar.blade.php
+++ resources/views/codificar.blade.php
@@ -3,78 +3,120 @@
 @section('content')
 
 <div class="container">
-        <form 
-            action="{{route('imagesupload')}}" 
-            method="POST" 
-            class="form"
-            enctype="multipart/form-data"
-        >
-            @csrf
-            <span>Faça o upload de uma imagem: </span>
-            <hr>
-            <div class="row">
-                <div class="col-6">
-                    <input type="text" name="name">
-                    <input type="file" id="file" name="image" required/>
-                </div>
-                <div class="col-6">
-                    <div class="card" style="width: 50rem; border:none;">
-                        <img id="preview" class="card-img-top" />
-                    </div>
-                </div>
-            </div>
-            <br>
-            <br>
-            <div class="row">
-                <div class="col-6">
-                    <span>Mensagem: </span>
-                    <hr>
-                    <div class="col">
-                        <div id="choose" class="row">
-                            <div class="col-8">
-                                <textarea 
-                                    id="message" 
-                                    class="form-control"
-                                    maxlength='1000'
-                                    placeholder='Escreva a mensagem que deseja ocultar.'
-                                    required
-                                >
-                                </textarea>
-                            </div>
-                        </div>
-                        <span>Senha:</span>
-                        <div class="row">
-                            <div class="col-4">
-                                <input 
-                                    id="password"
-                                    type="password" 
-                                    placeholder='Senha'
-                                    required
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    {{-- <span>Resultado</span> --}}
-                    <canvas id='canvas'></canvas>
-                    <div id='resulta'>
-                        <div class="card" style="width: 50rem; border:none;">
-                            <img id="output" class="card-img-top" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <br>
-            <br>
-            <button 
-                id="encode" 
-                class="btn btn-outline-primary"
-                type="submit" 
+    <div class="row w-100">
+        <div class="col-6">
+
+            <form 
+                action="{{route('imagesupload')}}" 
+                method="POST" 
+                class="form"
+                enctype="multipart/form-data"
             >
-                Ocultar Menssagem
-            </button>
-        </form>
+                @csrf
+                <span>Faça o upload de uma imagem: </span>
+                <hr>
+                <div class="row">
+                    <div class="col-6">
+                        <input type="text" name="name">
+                        <input type="file" id="file" name="image" required/>
+                    </div>
+                    <div class="col-6">
+                        <div class="card" style="width: 50rem; border:none;">
+                            <img id="preview" class="card-img-top" />
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="row">
+                    <div class="col-6">
+                        <span>Mensagem: </span>
+                        <hr>
+                        <div class="col">
+                            <div id="choose" class="row">
+                                <div class="col-8">
+                                    <textarea 
+                                        id="message" 
+                                        class="form-control"
+                                        maxlength='1000'
+                                        placeholder='Escreva a mensagem que deseja ocultar.'
+                                        required
+                                    >
+                                    </textarea>
+                                </div>
+                            </div>
+                            <span>Senha:</span>
+                            <div class="row">
+                                <div class="col-4">
+                                    <input 
+                                        id="password"
+                                        type="password" 
+                                        placeholder='Senha'
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        {{-- <span>Resultado</span> --}}
+                        <canvas id='canvas'></canvas>
+                        <div id='resulta'>
+                            <div class="card" style="width: 50rem; border:none;">
+                                <img id="output" class="card-img-top" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <button 
+                    id="encode" 
+                    class="btn btn-outline-primary"
+                    type="submit" 
+                >
+                    Ocultar Menssagem
+                </button>
+            </form>
+        </div>
+        <div class="col-6">
+                <span>Decodificar </span>
+                <hr>
+                <div class="row">
+                    <div class="col-6">
+                        <span>Mostrar Dados </span>
+                        <hr>
+                        <div class="col">
+                            <div id="choose" class="row">
+                            <span>Senha:</span>
+                            <div class="col-6">
+                                <input 
+                                    type='password' 
+                                    id='password2' 
+                                    class='password'
+                                    placeholder='Senha' 
+                                />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div id='reveal' class="col-6">
+                        <span>Mensagem oculta:</span>
+                        <div id='messageDecoded'></div>
+                    </div>
+                <br>
+                <br>
+                <button id='encode' class='submit' hidden></button>
+                <button 
+                    id="decode" 
+                    class="submit"
+                >
+                    Revelar Mensagem
+                </button>
+        </div>
+    </div>
 </div>	
 
 @endsection
--- resources/views/decodificar.blade.php
+++ resources/views/decodificar.blade.php
@@ -4,61 +4,7 @@
 
 <div class="container">
       
-            <span>Faça o upload de uma imagem: </span>
-            <hr>
-            <div class="row">
-                <div class="col-6">
-                    <input type="text" name="name">
-                    <input type="file" id="file" name="image" required/>
-                </div>
-                <div class="col-6">
-                    <div class="card" style="width: 50rem; border:none;">
-                        <img id="preview" class="card-img-top" />
-                    </div>
-                </div>
-            </div>
-            <br>
-            <br>
-            <div class="row">
-                <div class="col-6">
-                    <span>Mostrar Dados </span>
-                    <hr>
-                    <div class="col">
-                        <div id="choose" class="row">
-                        <span>Senha:</span>
-                        <div class="col-6">
-                            <input 
-                                type='password' 
-                                id='password2' 
-                                class='password'
-                                placeholder='Senha' 
-                            />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div id='reveal' class="col-6">
-                    <span>Mensagem oculta:</span>
-                    <div id='messageDecoded'></div>
-                </div>
-                <canvas id='canvas'></canvas>
-                <div id='resulta'>
-                    <div class="card" style="width: 50rem; border:none;">
-                        <img id="output" class="card-img-top" />
-                    </div>
-                </div>
-            </div>
-            <br>
-            <br>
-            <button id='encode' class='submit' hidden></button>
-            <button 
-                id="decode" 
-                class="submit"
-            >
-                Revelar Mensagem
-            </button>
+           
 </div>	
 
 @endsection
